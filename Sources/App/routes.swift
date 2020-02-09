6f65d4c6af72efb54f8d9cda11ad7c5da18d9315
Andrei Blaj <andreiblajdev@gmail.com>
2020-02-08 21:34:00 -0500

    added rockets

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,19 +2,63 @@ import Vapor
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    // Basic "It works" example
-    router.get { req in
-        return "It works!"
+    
+    router.get("/") { request -> Future<View> in
+        
+        let homeData = HomeData(
+            title: "Home"
+        )
+        
+        return try request.view().render("home", homeData)
     }
     
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
+    router.get("/rockets") { request -> Future<View> in
+        
+        let falcon1 = Rocket(
+            name: "Falcon 1",
+            imageUrl: "https://img.icons8.com/plasticine/2x/rocket.png",
+            active: false
+        )
+        
+        let falcon9 = Rocket(
+            name: "Falcon 9",
+            imageUrl: "https://img.icons8.com/plasticine/2x/rocket.png",
+            active: true
+        )
+        
+        let falconHeavy = Rocket(
+            name: "Falcon Heavy",
+            imageUrl: "https://img.icons8.com/plasticine/2x/rocket.png",
+            active: true
+        )
+        
+        let starship = Rocket(
+            name: "Starship",
+            imageUrl: "https://img.icons8.com/plasticine/2x/rocket.png",
+            active: false
+        )
+        
+        let rocketsData = RocketsData(
+            title: "Rockets",
+            rocketList: [falcon1, falcon9, falconHeavy, starship]
+        )
+        
+        return try request.view().render("rockets", rocketsData)
     }
+    
+}
 
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+struct Rocket: Encodable {
+    let name: String
+    let imageUrl: String
+    let active: Bool
+}
+
+struct RocketsData: Encodable {
+    let title: String
+    let rocketList: [Rocket]
+}
+
+struct HomeData: Encodable {
+    let title: String
 }
